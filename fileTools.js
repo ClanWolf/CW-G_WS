bf0af7089bb647c10e2f67cbaef9cae052be1f42
WarWolfen <warwolfen@gmail.com>
2026-05-30 00:26:53 +0200

    Removed vscode specific config files

--- fileTools.js
+++ fileTools.js
@@ -1,24 +1,25 @@
 const fs = require('fs').promises;
 const path = require('path');
-const {logger} = require("./logger");
+const { logger} = require("./logger");
 
 async function listDir(dirPath) {
-    try {
-        const logfiles = [ ];
-        const files = await fs.readdir(dirPath);
-        const statPromises = files.map(async (file) => {
-            const fullPath = path.join(dirPath, file);
-            const stats = await fs.stat(fullPath);
-            if (stats.isFile() && file.startsWith("combined.")) {
-                logfiles.push(`${file}`);
-            }
-        });
-        await Promise.all(statPromises);
-        return logfiles;
-    } catch (err) {
-        console.error("Error reading directory:", err);
-	    logger.error(`Error reading directory: ${err}`);
-    }
+  try {
+    const logfiles = [ ];
+    const files = await fs.readdir(dirPath);
+    const statPromises = files.map(async (file) => {
+      const fullPath = path.join(dirPath, file);
+      const stats = await fs.stat(fullPath);
+
+      if (stats.isFile() && file.startsWith("combined.")) {
+        logfiles.push(`${file}`);
+      }
+    });
+    await Promise.all(statPromises);
+    return logfiles;
+  } catch (err) {
+    console.error("Error reading directory:", err);
+    logger.error(`Error reading directory: ${err}`);
+  }
 }
 
 module.exports = listDir;
